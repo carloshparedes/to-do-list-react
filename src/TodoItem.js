0e891cdd547790aa8af7193443753dd246e0cacc
Carlos Hernandez <carloshparedes@gmail.com>
2024-10-11 10:44:42 +0200

    Add basic components for todo functionality

---
+++ src/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TodoItem() {
+    return (
+        <li>
+            <span>V</span>
+            <p>Card name</p>
+            <span>X</span>
+        </li>
+    );
+}
+
+export { TodoItem };
